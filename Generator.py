144b05fe61c545e7220c6df616706eda90a8da2d
Akhil Reddy <avr54@scarletmail.rutgers.edu>
2022-08-09 19:20:04 -0400

    Finalized changes

--- Generator.py
+++ Generator.py
@@ -2,13 +2,13 @@
 # it is the backend for the main file, we are passed in total length we want and then we can get a password from that 
 
 import random
-import string
 import math
+#import string
 
 # let's take the password to only be 10 characters long 
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-numerics = "0123456789"
+numerics = "0123456789" 
 specialChars = "@#$%&*"
 
 print('This will be your alphanumeric password length')
@@ -24,13 +24,32 @@ password = [] #new list has to be created to store the password
 
 # we need to generate ranodm parts of each
 
-def generate_pass(length, array, is_alpha=False):
+def passwordGenerator(length, array, isAlpha):
     for i in range(length):
         index = random.randint(0, len(array) - 1)
         character = array[index]
-        if is_alpha:
+        if isAlpha:
             case = random.randint(0, 1)
             if case == 1:
                 character = character.upper()
         password.append(character)
 
+# alphabet part of the password
+passwordGenerator(alphabetLength, alphabet, True)
+
+# numeric password
+passwordGenerator(numericsLength, numerics, False)
+
+# special Character password
+passwordGenerator(specialcharsLength, specialChars, False)
+
+# suffle the generated password list. TWICE because of more 
+random.shuffle(password)  
+random.shuffle(password)
+
+# convert List To string since the list is currently not a string
+gen_password = ""
+
+for i in password:
+    gen_password = gen_password + str(i)
+print(gen_password)
